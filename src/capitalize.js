134d0a18da72e20983702987150fd6a2ef234890
birando <birger1@live.com>
2024-09-05 12:08:43 +0200

    feat: add deal with invalid input in capitalize func

--- src/capitalize.js
+++ src/capitalize.js
@@ -2,6 +2,7 @@
 // * returns it with the first character capitalized.
 
 export function capitalize(string) {
+  if (typeof string != "string") return "invalid input";
   const [firstLetter, ...rest] = string.split("");
   return firstLetter.toUpperCase() + rest.join("");
 }
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -2,6 +2,9 @@ import { it, describe, expect } from "vitest";
 import { capitalize } from "../src/capitalize";
 
 describe("capitalize", () => {
+  it("should return error message if input isn't a string", () => {
+    expect(capitalize(10)).toBe("invalid input");
+  });
   it("should return the string with the first letter capitalized if all letters are lower case", () => {
     expect(capitalize("random")).toBe("Random");
   });
