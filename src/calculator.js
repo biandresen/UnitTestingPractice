cfb154723e68c21c997796b6493ba27582418099
birando <birger1@live.com>
2024-09-05 12:03:54 +0200

    refactor: calculator func

--- src/calculator.js
+++ src/calculator.js
@@ -3,20 +3,23 @@
 // * should take two numbers and return the correct calculation.
 
 export const calculator = {
+  inputIsInvalid(n1, n2) {
+    if (typeof n1 != "number" || typeof n2 != "number") return true;
+  },
   add(n1, n2) {
-    if (typeof n1 != "number" || typeof n2 != "number") return null;
-    return n1 + n2;
+    if (this.inputIsInvalid(n1, n2)) return null;
+    else return n1 + n2;
   },
   subtract(n1, n2) {
-    if (typeof n1 != "number" || typeof n2 != "number") return null;
-    return n1 - n2;
+    if (this.inputIsInvalid(n1, n2)) return null;
+    else return n1 - n2;
   },
   divide(n1, n2) {
-    if (typeof n1 != "number" || typeof n2 != "number") return null;
-    return n1 / n2;
+    if (this.inputIsInvalid(n1, n2)) return null;
+    else return n1 / n2;
   },
   multiply(n1, n2) {
-    if (typeof n1 != "number" || typeof n2 != "number") return null;
-    return n1 * n2;
+    if (this.inputIsInvalid(n1, n2)) return null;
+    else return n1 * n2;
   },
 };
